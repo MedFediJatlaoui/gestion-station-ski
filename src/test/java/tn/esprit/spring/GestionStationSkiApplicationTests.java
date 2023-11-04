fad73a35dabf403db356e2875e13a0f2e99e71f9
malekbenrabah <malek.benrabah2@gmail.com>
2023-11-04 15:54:32 +0100

    exclude db test

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -3,7 +3,8 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class GestionStationSkiApplicationTests {
 
 	@Test
