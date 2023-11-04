41e3ebb878e19bb0fe09301977f7a0aeb22d2072
malekbenrabah <malek.benrabah2@gmail.com>
2023-11-04 16:03:09 +0100

    update test class

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,10 +2,11 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.repositories.IInstructorRepository;
 import tn.esprit.spring.services.*;
 
 //@SpringBootTest
-@SpringBootTest(classes = {CourseServicesImpl.class, InstructorServicesImpl.class , PisteServicesImpl.class, RegistrationServicesImpl.class, SkierServicesImpl.class, SubscriptionServicesImpl.class }, properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
+@SpringBootTest(classes = {CourseServicesImpl.class, ICourseServices.class,InstructorServicesImpl.class, IInstructorRepository.class , PisteServicesImpl.class, RegistrationServicesImpl.class, SkierServicesImpl.class, SubscriptionServicesImpl.class }, properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class GestionStationSkiApplicationTests {
 
 	@Test
