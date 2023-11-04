967bc1da7a3d4d435062d6e3a1232e8a299909a8
malekbenrabah <malek.benrabah2@gmail.com>
2023-11-04 16:09:31 +0100

    bbb

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -6,7 +6,6 @@ import tn.esprit.spring.repositories.IInstructorRepository;
 import tn.esprit.spring.services.*;
 
 //@SpringBootTest
-@SpringBootTest(classes = {CourseServicesImpl.class, ICourseServices.class,InstructorServicesImpl.class, IInstructorRepository.class , PisteServicesImpl.class, RegistrationServicesImpl.class, SkierServicesImpl.class, SubscriptionServicesImpl.class }, properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class GestionStationSkiApplicationTests {
 
 	@Test
