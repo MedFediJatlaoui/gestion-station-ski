ebc2e3325faae1e33b313584cf1b5db6bd9e5048
MedFediJatlaoui <mohamedfedi.jatlaoui@esprit.tn>
2023-11-07 01:27:16 +0100

    JUNIT5

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,13 +1,27 @@
 package tn.esprit.spring;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Piste;
+import tn.esprit.spring.repositories.IPisteRepository;
+import tn.esprit.spring.services.PisteServicesImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class GestionStationSkiApplicationTests {
 
--- src/test/java/tn/esprit/spring/TestPisteServicesImpl.java
+++ src/test/java/tn/esprit/spring/TestPisteServicesImpl.java
@@ -1,20 +1,19 @@
 package tn.esprit.spring;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repositories.IPisteRepository;
-import tn.esprit.spring.services.IPisteServices;
 import tn.esprit.spring.services.PisteServicesImpl;
 
-import java.time.LocalDate;
 import java.util.*;
 import java.util.ArrayList;
 
@@ -22,17 +21,20 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 @ExtendWith(MockitoExtension.class)
-public class PisteServicesImplTest {
+
+public class TestPisteServicesImpl {
     @Mock
     private IPisteRepository pisteRepository;
 
 
     @InjectMocks
     private PisteServicesImpl pisteServices;
+
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
+
     @Test
     public void testRetrieveAllPistes() {
 
